c770aaa18a105ded8fdcecfe1da07a4c04e8f899
Cheng-Lung Sung <clsung@gmail.com>
2013-06-04 11:45:14 +0800

    bump to 0.1.1

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+test_requirements = ['nose', 'mock', 'testfixtures', 'httpretty']
 config = {
     'description': 'Google Drive API Wrapper',
     'author': 'Cheng-Lung Sung',
     'url': 'https://github.com/clsung/gdapi',
     'download_url': 'http://pypi.python.org/pypi/gdapi',
     'author_email': 'clsung@gmail.com',
-    'version': '0.1.0',
-    'install_requires': ['nose', 'mock', 'requests'],
+    'version': '0.1.1',
+    'test_requires': test_requirements,
+    'install_requires': ['requests'],
     'packages': ['gdapi'],
     'scripts': ['script/download_to_somewhere.py',
                 'script/upload_to_root.py'],
