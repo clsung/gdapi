885276eb0949babe93ccbcd1c58d987ac29f9108
Cheng-Lung Sung <clsung@gmail.com>
2013-06-04 18:26:31 +0800

    bump to 0.1.4 with support

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'url': 'https://github.com/clsung/gdapi',
     'download_url': 'http://pypi.python.org/pypi/gdapi',
     'author_email': 'clsung@gmail.com',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'test_requires': test_requirements,
     'install_requires': ['requests'],
     'packages': ['gdapi'],
